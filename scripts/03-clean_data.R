832e8dc738ba09ff602c332c6445eee24824f3f9
Sakura Noskor <sakura.noskor@mail.utoronto.ca>
2024-11-21 15:46:08 -0500

    update scripts and sketches

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,44 +1,18 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Shreya Sakura Noskor
+# Purpose: Cleans and saves the data from spotify on Coldplay
+# Author: Shreya Sakura Noskor, Cristina Burca, Aamishi Sundeep Avarsekar
 # Date: 10 October 2024
-# Contact: sakura.noskor@mail.utoronto.ca
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 #### Workspace setup ####
 library(tidyverse)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+coldplay <- readRDS("data/coldplay.rds")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+coldplay_clean <- coldplay %>%
+  select(track_name, album_name,duration_ms, album_release_date, tempo, energy, loudness) %>%
+  filter(!is.na(album_release_date))
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(coldplay_clean, "data/coldplay_cleaned.csv")
+saveRDS(coldplay_clean, "data/coldplay_cleaned.rds")
